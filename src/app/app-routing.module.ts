963c3e99bd5f15eb76f474185e964fe5069e23b9
Sergio Flores <sergioflores.vt@gmail.com>
2023-07-22 21:20:02 -0300

    primera version de inventario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardRoutingModule } from './modules/dashboard/dashboard-routing.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
+];
+/*Cuando la URL coincide exactamente con la ruta de acceso vacía, 
+se redirige al URL /dashboard. Esto significa que cuando los usuarios
+ visiten la raíz de la aplicación (por ejemplo, http://tu-sitio.com/), 
+ serán automáticamente redirigidos a http://tu-sitio.com/dashboard.
+ */
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, { enableTracing: false, useHash: true }),
+    DashboardRoutingModule,
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
